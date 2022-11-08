09078a8b31ae0afe5e804794cfb3033aa7902893
Razmik <razmik.unanyan@mail.ru>
2022-11-08 15:51:09 +0400

    feat: category

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, FC, memo, useCallback, useEffect, useState } from 'react'
 import style from './Header.module.scss'
 import cn from 'classnames'
-import { useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { ReactComponent as FilterIcon } from '../../assets/svg/filter.svg'
 import { HeaderProps } from './Header.props'
 import { Button, Input, Menu, Switch, NavBar, Select } from '../../components'
@@ -9,7 +9,7 @@ import { useDebounce } from '../../hooks/fakestore.hooks'
 import { useGetCategoriesQuery } from '../../redux/apiSlice'
 
 const Header: FC<HeaderProps> = memo<HeaderProps>(({ theme, setTheme, ...props }) => {
-  const { category } = useParams()
+  const { pathname } = useLocation()
 
   const { data: categories = [] } = useGetCategoriesQuery()
 
@@ -19,13 +19,13 @@ const Header: FC<HeaderProps> = memo<HeaderProps>(({ theme, setTheme, ...props }
 
   const debouncedSearchTerm = useDebounce(searchValue, 500)
 
-  console.log(category)
-
   useEffect(() => {
     if (debouncedSearchTerm !== '') {
       console.log(debouncedSearchTerm)
     }
   }, [debouncedSearchTerm])
+
+  const category = pathname === '/' ? 'all' : pathname.replace(/[%/0-9]/gi, ' ')
 
   const handleSetSearchValue = (e: ChangeEvent<HTMLInputElement>): void => setSearchValue(e.target.value)
   const handleSetOpen = useCallback((): void => setOpen(prev => !prev), [])
@@ -46,7 +46,7 @@ const Header: FC<HeaderProps> = memo<HeaderProps>(({ theme, setTheme, ...props }
             )}
             <div className={style.headerBottom}>
                 <div className={style.categories}>
-                    {category ?? 'all'}
+                    {category.toUpperCase()}
                 </div>
                 <div className={style.buttons}>
                     <Switch onClick={setTheme} theme={theme}/>
